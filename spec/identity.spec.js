cff765c951988f816e7e09fbf2378081703ca954
Hoofman <hoofman@sdf-eu.org>
2016-10-25 11:46:09 +0100

    identity is a function; takes one arg; retutns unmodified input

---
+++ spec/identity.spec.js
@@ -0,0 +1,25 @@
+var chai=require('chai');
+var expect=chai.expect;
+var identity=require('../lib/identity');
+
+describe('identity',function(){
+
+    it('it is a func', function(){
+        expect(identity).to.be.a('function');
+    })
+    it('it takes on argument', function(){
+        expect(identity.length).to.equal(1);
+    })
+    it('it returns the argument pased to it', function(){
+        expect(identity('input')).to.equal('input');
+    })
+    it('it does not modify the argument pased to it', function(){
+        expect(typeof identity(1)).to.equal('number');
+    })
+    it('it does not create new objects', function(){
+        var myArray=[1,2,3];
+        expect( identity(myArray)).to.equal(myArray);
+    })
+
+
+   })
