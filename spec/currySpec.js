ce3c4a0862e2f366f633a491b7d6be7c439511a1
Hoofman <hoofman@sdf-eu.org>
2016-10-26 12:31:19 +0100

    [inc] passing tests using curry()

--- spec/currySpec.js
+++ spec/currySpec.js
@@ -2,7 +2,8 @@
 var chai = require('chai');
 var expect = chai.expect;
 var curry = require('../lib/curry.js').curry;
-var mul = require('../lib/curry.js').mul;
+var mul = require('../lib/binFunc.js').mul;
+var add = require('../lib/binFunc.js').add;
 
 describe('curry',function(){
   it('it is a func', function () {
@@ -15,4 +16,8 @@ describe('curry',function(){
       var curriedMul = curry (mul, 6);
     expect(curriedMul(3)).to.equal(mul(3,6));
     });
+  it('should add two arguments', function () {
+    var curriedAdd = curry (add, 6);
+    expect(curriedAdd(3)).to.equal(add(3,6));
+  });
 });
--- spec/incSpec.js
+++ spec/incSpec.js
@@ -1,7 +1,7 @@
 var chai=require('chai');
 var expect = chai.expect;
 var inc =require('../lib/inc.js').inc;
-
+var add = require('../lib/binFunc').add;
 //var identity=require('../lib/identity.js').identity
 
 xdescribe('inc using addf()',function() {
@@ -18,7 +18,7 @@ xdescribe('inc using addf()',function() {
 });
 
 
-describe('inc using liftf()',function() {
+xdescribe('inc using liftf()',function() {
     it('it is a func', function () {
         expect(inc).to.be.a('function');
     });
@@ -31,9 +31,19 @@ describe('inc using liftf()',function() {
 
 })
 
-/*
-console.log('**************')
+describe('inc using curry()',function() {
+    it('it is a func', function () {
+        expect(inc).to.be.a('function');
+    });
 
-console.log(inc(function (a, b) { return a + b; })(5)(1))
-console.log('**************')
-*/
+    it('inc(9)(1) should equal var i=9;++i', function () {
+        var i=9;
+        var incCurry = inc(add, 1);
+        expect(incCurry(9)).to.equal(++i);});
+
+    it('inc(5)(1) should equal 6', function () {
+        var incCurry = inc(add, 1);
+        expect(incCurry(5)).to.equal(6);});
+
+})
+
